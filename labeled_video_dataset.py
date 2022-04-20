b826a3c6c44b772ddf6a7d149042d0996d75c41f
louis-4 <luchongkai96@gmail.com>
2022-04-20 19:26:20 +0800

    add slowfast_mvit.py

---
+++ labeled_video_dataset.py
@@ -0,0 +1,239 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type
+
+import torch.utils.data
+from iopath.common.file_io import g_pathmgr
+from pytorchvideo.data.clip_sampling import ClipSampler
+from pytorchvideo.data.frame_video import FrameVideo
+from pytorchvideo.data.labeled_video_dataset import LabeledVideoDataset
+from pytorchvideo.data.labeled_video_paths import LabeledVideoPaths
+from pytorchvideo.data.utils import MultiProcessSampler
+from pytorchvideo.data.video import VideoPathHandler
+
+logger = logging.getLogger(__name__)
+
+
+class FrameVideo2(FrameVideo):
+
+    @classmethod
+    def from_directory(
+            cls,
+            path: str,
+            fps: float = 30.0,
+            multithreaded_io=False,
+            path_order_cache: Optional[Dict[str, List[str]]] = None,
+            filename_tmpl=None,
+    ):
+        if path_order_cache is not None and path in path_order_cache:
+            return cls.from_frame_paths(path_order_cache[path], fps, multithreaded_io)
+
+        assert g_pathmgr.isdir(path), f"{path} is not a directory"
+        rel_frame_paths = sorted(list(Path(path).glob(filename_tmpl)))
+
+        frame_paths = [str(p) for p in rel_frame_paths]
+        if path_order_cache is not None:
+            path_order_cache[path] = frame_paths
+        return cls.from_frame_paths(frame_paths, fps, multithreaded_io)
+
+
+class VideoPathHandler2(VideoPathHandler):
+
+    def video_from_path(self, filepath, decode_audio=False, decoder="pyav", fps=30, filename_tmpl='img_*.jpg'):
+        try:
+            is_file = g_pathmgr.isfile(filepath)
+            is_dir = g_pathmgr.isdir(filepath)
+        except NotImplementedError:
+
+            # Not all PathManager handlers support is{file,dir} functions, when this is the
+            # case, we default to assuming the path is a file.
+            is_file = True
+            is_dir = False
+
+        if is_file:
+            from pytorchvideo.data.encoded_video import EncodedVideo
+
+            return EncodedVideo.from_path(filepath, decode_audio, decoder)
+        elif is_dir:
+
+            assert not decode_audio, "decode_audio must be False when using FrameVideo"
+            return FrameVideo2.from_directory(
+                filepath, fps, path_order_cache=self.path_order_cache, filename_tmpl=filename_tmpl
+            )
+        else:
+            raise FileNotFoundError(f"{filepath} not found.")
+
+
+class LabeledVideoDataset2(LabeledVideoDataset):
+    def __init__(self, *args, **kwargs):
+        super(LabeledVideoDataset2, self).__init__(*args, **kwargs)
+        self.video_path_handler = VideoPathHandler2()
+
+    def __next__(self) -> dict:
+        if not self._video_sampler_iter:
+            # Setup MultiProcessSampler here - after PyTorch DataLoader workers are spawned.
+            self._video_sampler_iter = iter(MultiProcessSampler(self._video_sampler))
+
+        for i_try in range(self._MAX_CONSECUTIVE_FAILURES):
+            # Reuse previously stored video if there are still clips to be sampled from
+            # the last loaded video.
+            if self._loaded_video_label:
+                video, info_dict, video_index = self._loaded_video_label
+            else:
+                video_index = next(self._video_sampler_iter)  # return StopIteration when all video used
+                try:
+                    video_path, info_dict = self._labeled_videos[video_index]
+                    video = self.video_path_handler.video_from_path(
+                        video_path,
+                        decode_audio=self._decode_audio,
+                        decoder=self._decoder,
+                    )
+                    self._loaded_video_label = (video, info_dict, video_index)
+                except Exception as e:
+                    logger.debug(
+                        "Failed to load video with error: {}; trial {}".format(
+                            e,
+                            i_try,
+                        )
+                    )
+                    continue
+            (
+                clip_start,
+                clip_end,
+                clip_index,
+                aug_index,
+                is_last_clip,
+            ) = self._clip_sampler(
+                self._next_clip_start_time, video.duration, info_dict
+            )
+
+            if isinstance(clip_start, list):  # multi-clip in each sample
+
+                # Only load the clips once and reuse previously stored clips if there are multiple
+                # views for augmentations to perform on the same clips.
+                if aug_index[0] == 0:
+                    self._loaded_clip = {}
+                    loaded_clip_list = []
+                    for i in range(len(clip_start)):
+                        clip_dict = video.get_clip(clip_start[i], clip_end[i])
+                        if clip_dict is None or clip_dict["video"] is None:
+                            self._loaded_clip = None
+                            break
+                        loaded_clip_list.append(clip_dict)
+
+                    if self._loaded_clip is not None:
+                        for key in loaded_clip_list[0].keys():
+                            self._loaded_clip[key] = [x[key] for x in loaded_clip_list]
+
+            else:  # single clip case
+
+                # Only load the clip once and reuse previously stored clip if there are multiple
+                # views for augmentations to perform on the same clip.
+                if aug_index == 0:
+                    self._loaded_clip = video.get_clip(clip_start, clip_end)
+
+            self._next_clip_start_time = clip_end
+
+            video_is_null = (
+                    self._loaded_clip is None or self._loaded_clip["video"] is None
+            )
+            if (
+                    is_last_clip[-1] if isinstance(is_last_clip, list) else is_last_clip
+            ) or video_is_null:
+                # Close the loaded encoded video and reset the last sampled clip time ready
+                # to sample a new video on the next iteration.
+                self._loaded_video_label[0].close()
+                self._loaded_video_label = None
+                self._next_clip_start_time = 0.0
+                self._clip_sampler.reset()
+                if video_is_null:
+                    logger.debug(
+                        "Failed to load clip {}; trial {}".format(video.name, i_try)
+                    )
+                    continue
+
+            frames, frame_indices = self._loaded_clip["video"], self._loaded_clip["frame_indices"]
+            audio_samples = self._loaded_clip["audio"]
+            sample_dict = {
+                "duration": video.duration,
+                "fps": video._fps,
+                "video": frames,
+                "frame_indices": torch.tensor(frame_indices, dtype=torch.long),
+                "video_name": video.name,
+                "video_index": video_index,
+                "clip_index": clip_index,
+                "aug_index": aug_index,
+                **info_dict,
+                **({"audio": audio_samples} if audio_samples is not None else {}),
+            }
+            if self._transform is not None:
+                sample_dict = self._transform(sample_dict)
+
+                # User can force dataset to continue by returning None in transform.
+                if sample_dict is None:
+                    continue
+
+            return sample_dict
+        else:
+            raise RuntimeError(
+                f"Failed to load video after {self._MAX_CONSECUTIVE_FAILURES} retries."
+            )
+
+
+def labeled_video_dataset(
+        data_path: str,
+        clip_sampler: ClipSampler,
+        video_sampler: Type[torch.utils.data.Sampler] = torch.utils.data.RandomSampler,
+        transform: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+        video_path_prefix: str = "",
+        decode_audio: bool = True,
+        decoder: str = "pyav",
+) -> LabeledVideoDataset:
+    """
+    A helper function to create ``LabeledVideoDataset`` object for Ucf101 and Kinetics datasets.
+
+    Args:
+        data_path (str): Path to the data. The path type defines how the data
+            should be read:
+
+            * For a file path, the file is read and each line is parsed into a
+              video path and label.
+            * For a directory, the directory structure defines the classes
+              (i.e. each subdirectory is a class).
+
+        clip_sampler (ClipSampler): Defines how clips should be sampled from each
+                video. See the clip sampling documentation for more information.
+
+        video_sampler (Type[torch.utils.data.Sampler]): Sampler for the internal
+                video container. This defines the order videos are decoded and,
+                if necessary, the distributed split.
+
+        transform (Callable): This callable is evaluated on the clip output before
+                the clip is returned. It can be used for user defined preprocessing and
+                augmentations to the clips. See the ``LabeledVideoDataset`` class for clip
+                output format.
+
+        video_path_prefix (str): Path to root directory with the videos that are
+                loaded in ``LabeledVideoDataset``. All the video paths before loading
+                are prefixed with this path.
+
+        decode_audio (bool): If True, also decode audio from video.
+
+        decoder (str): Defines what type of decoder used to decode a video.
+
+    """
+    labeled_video_paths = LabeledVideoPaths.from_path(data_path)
+    labeled_video_paths.path_prefix = video_path_prefix
+    dataset = LabeledVideoDataset(
+        labeled_video_paths,
+        clip_sampler,
+        video_sampler,
+        transform,
+        decode_audio=decode_audio,
+        decoder=decoder,
+    )
+    return dataset
---
+++ pytorchvideo_for_TemporalActionProposal.py
@@ -0,0 +1,299 @@
+import os
+import random
+import warnings
+
+import pytorch_lightning
+import pytorchvideo.data
+import pytorchvideo.models.resnet
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+import torchmetrics
+from pytorchvideo.data.clip_sampling import ClipInfo, ClipSampler
+from pytorchvideo.transforms import (
+    ApplyTransformToKey,
+    Normalize,
+    ShortSideScale,
+    RandomShortSideScale,
+    UniformTemporalSubsample,
+)
+from pytorchvideo.transforms.functional import uniform_temporal_subsample
+from torchvision.transforms import (
+    Compose,
+    Lambda,
+    RandomCrop,
+    RandomHorizontalFlip,
+)
+from torchvision.transforms._transforms_video import CenterCropVideo
+
+from labeled_video_dataset import LabeledVideoDataset2
+
+warnings.filterwarnings(
+    "ignore", ".*Trying to infer the `batch_size` from an ambiguous collection.*"
+)
+
+
+class BBoxesClipSampler(ClipSampler):
+
+    def __init__(self, clip_duration):
+        super(BBoxesClipSampler, self).__init__(clip_duration)
+
+    def __call__(self, last_clip_time, video_duration, annotation):
+        # annotation['label'] = ([start_1, end_1, label_1], [start_2, end_2, label_2], ...)
+        start, end, _ = random.choice(annotation['label'])
+        start, end = start.item(), end.item()
+        duration = end - start
+
+        max_possible_clip_start = min(end + duration / 2 - self._clip_duration,
+                                      video_duration - self._clip_duration)
+        max_possible_clip_start = max(max_possible_clip_start, 0)
+        min_possible_clip_start = min(start - duration / 2,
+                                      video_duration - self._clip_duration)
+        min_possible_clip_start = max(min_possible_clip_start, 0)
+        clip_start_sec = random.uniform(min_possible_clip_start, max_possible_clip_start)
+        return ClipInfo(clip_start_sec,
+                        clip_start_sec + self._clip_duration,
+                        clip_index=0,
+                        aug_index=0,
+                        is_last_clip=True)
+
+
+class SpatialPool(nn.Module):
+
+    def __init__(self, thw=(8, 7, 7)):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.thw = thw
+
+    def forward(self, x):
+        B, N, C = x.shape
+        # [B, THW, C]
+        x = x.transpose(1, 2)
+        # [B, C, THW]
+        x = x.reshape(B, C, *self.thw)
+        # [B, C, T, H, W]
+        x = self.avgpool(x).flatten(start_dim=2).transpose(1, 2)
+        # [B, T, C]
+        return x
+
+
+def THUMOS14(
+        ann_file,
+        clip_sampler,
+        video_sampler=torch.utils.data.RandomSampler,
+        transform=None,
+        video_path_prefix=""):
+    # Populate keyframes list
+    fps = 30
+    labeled_video_paths = []
+    ann = {}
+    with open(ann_file, 'r') as f:
+        for line in f.readlines():
+            video_name, video_len, start_label, end_label, category_label = line.rstrip('\n').split(',')
+            if int(video_len) < 59:
+                continue
+            video_frames_dir = os.path.join(video_path_prefix, video_name.rsplit('.', 1)[0])
+            ann.setdefault(video_name, {}).setdefault('frame_dir', video_frames_dir)
+            ann.setdefault(video_name, {}).setdefault('video_len', int(video_len) + 1)
+            ann.setdefault(video_name, {}).setdefault('label', []).append(
+                [int(start_label) / fps, int(end_label) / fps, int(category_label)])
+    for video_ann in ann.values():
+        # labels = np.zeros(int(video_ann['video_len'] + 1))
+        # for box in video_ann['boxes']:
+        #     labels[box[0]: box[1]] = 1
+        # labels = {"label": torch.from_numpy(labels).float()}
+        labeled_video_paths.append((video_ann['frame_dir'], {"label": torch.tensor(video_ann['label'])}))
+
+    return LabeledVideoDataset2(
+        labeled_video_paths=labeled_video_paths,
+        clip_sampler=clip_sampler,
+        transform=transform,
+        video_sampler=video_sampler,
+        decode_audio=False,
+    )
+
+
+class ClipLabels(torch.nn.Module):
+    def __init__(self, clip_len=60, frames_per_clip=16, fps=30, temporal_scale=1):
+        super().__init__()
+        self.clip_len = clip_len
+        self.frames_per_clip = frames_per_clip
+        self.fps = fps
+        self.t_scale = temporal_scale
+
+    def get_binary_label(self, frame_indices, annotations):
+        binary = torch.zeros_like(frame_indices)
+        for ann in annotations:
+            binary = binary.where((frame_indices < ann[0] * self.fps) | (frame_indices > ann[1] * self.fps),
+                                  torch.tensor(1))
+        binary_scaled = binary[::self.t_scale]
+        for i in range(1, self.t_scale):
+            binary_scaled = binary_scaled | binary[i::self.t_scale]
+        return binary_scaled.float()
+
+    def __call__(self, sample_dict):
+        # clip_len = len(sample_dict["frame_indices"])
+        # indices = torch.linspace(0, clip_len - 1, self.frames_per_clip)
+        # self.subsample_indices = torch.clamp(indices, 0, clip_len - 1).long()
+        # frame_indices_subsampled = torch.index_select(sample_dict["frame_indices"], dim=-1,
+        #                                               index=self.subsample_indices)
+        frame_indices_subsampled = uniform_temporal_subsample(sample_dict["frame_indices"], self.frames_per_clip, temporal_dim=0)
+        sample_dict['label'] = self.get_binary_label(frame_indices_subsampled, sample_dict["label"])
+        return sample_dict
+
+
+class THUMOS14DataModule(pytorch_lightning.LightningDataModule):
+    # Dataset configuration
+    _DATA_PATH = 'my_data/thumos14'
+    _ANN_FILE_TRAIN = 'annotations/apn/apn_val.csv'
+    _ANN_FILE_VAL = 'annotations/apn/apn_test_demo.csv'
+    _FPS = 30
+    _CLIP_DURATION = 3  # Duration of sampled clip for each video
+    _FRAMES_PER_CLIP = 16
+    _BATCH_SIZE = 4
+    _NUM_WORKERS = 8  # Number of parallel processes fetching data
+
+    def train_dataloader(self):
+        """
+        Create the Kinetics train partition from the list of video labels
+        in {self._DATA_PATH}/train
+        """
+        train_transform = Compose(
+            [
+                ClipLabels(clip_len=self._CLIP_DURATION * self._FPS, frames_per_clip=self._FRAMES_PER_CLIP),
+                ApplyTransformToKey(
+                    key="video",
+                    transform=Compose(
+                        [
+                            UniformTemporalSubsample(self._FRAMES_PER_CLIP),
+                            Lambda(lambda x: x / 255.0),
+                            Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                            RandomShortSideScale(min_size=256, max_size=320),
+                            RandomCrop(224),
+                            RandomHorizontalFlip(p=0.5),
+                        ]
+                    ),
+                ),
+
+            ]
+        )
+        train_dataset = THUMOS14(
+            ann_file=os.path.join(self._DATA_PATH, self._ANN_FILE_TRAIN),
+            clip_sampler=BBoxesClipSampler(self._CLIP_DURATION),
+            video_path_prefix=os.path.join(self._DATA_PATH, "rawframes/val"),
+            transform=train_transform
+        )
+        return torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=self._BATCH_SIZE,
+            num_workers=self._NUM_WORKERS,
+        )
+
+    def val_dataloader(self):
+        """
+        Create the Kinetics validation partition from the list of video labels
+        in {self._DATA_PATH}/val
+        """
+        val_transform = Compose(
+            [
+                ClipLabels(clip_len=self._CLIP_DURATION * self._FPS, frames_per_clip=self._FRAMES_PER_CLIP),
+                ApplyTransformToKey(
+                    key="video",
+                    transform=Compose(
+                        [
+                            UniformTemporalSubsample(self._FRAMES_PER_CLIP),
+                            Lambda(lambda x: x / 255.0),
+                            Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                            ShortSideScale(size=256),
+                            CenterCropVideo(crop_size=224)
+                        ]
+                    ),
+                ),
+            ]
+        )
+        val_dataset = THUMOS14(
+            ann_file=os.path.join(self._DATA_PATH, self._ANN_FILE_VAL),
+            clip_sampler=pytorchvideo.data.make_clip_sampler("uniform", self._CLIP_DURATION),
+            video_path_prefix=os.path.join(self._DATA_PATH, "rawframes/test"),
+            transform=val_transform
+        )
+        return torch.utils.data.DataLoader(
+            val_dataset,
+            batch_size=self._BATCH_SIZE,
+            # num_workers=self._NUM_WORKERS,
+            num_workers=4,
+        )
+
+
+def make_kinetics_mvit():
+    # pytorchvideo.models.vision_transformers.create_multiscale_vision_transformers()
+    # return pytorchvideo.models.resnet.create_resnet(
+    #     input_channel=3,  # RGB input from Kinetics
+    #     model_depth=50,  # For the tutorial let's just use a 50 layer network
+    #     model_num_class=400,  # Kinetics has 400 classes so we need out final head to align
+    #     norm=nn.BatchNorm3d,
+    #     activation=nn.ReLU,
+    # )
+    model = torch.hub.load("facebookresearch/pytorchvideo", model="mvit_base_16x4", pretrained=True)
+    # for m in model.modules():
+    #     if hasattr(m, 'cls_embed_on'):
+    #         m.cls_embed_on = False
+    #     if hasattr(m, 'has_cls_embed'):
+    #         m.has_cls_embed = False
+    # model.head = nn.Sequential(SpatialPool(thw=[8, 7, 7]), nn.Linear(768, 2), nn.Flatten(start_dim=1))
+    model.head.proj = nn.Linear(768, 16)
+    return model
+
+
+class VideoClassificationLightningModule(pytorch_lightning.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = make_kinetics_mvit()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        # The model expects a video tensor of shape (B, C, T, H, W), which is the
+        # format provided by the dataset
+        y_hat = self.model(batch["video"])
+
+        # Compute cross entropy loss, loss.backwards will be called behind the scenes
+        # by PyTorchLightning after being returned from this method.
+        loss = F.binary_cross_entropy_with_logits(y_hat, batch["label"])
+        accu = torchmetrics.functional.accuracy(y_hat, batch["label"].long())
+
+        # Log the train loss to Tensorboard
+        self.log("train_loss", loss.item())
+        self.log("train_accu", accu, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        y_hat = self.model(batch["video"])
+        loss = F.binary_cross_entropy_with_logits(y_hat, batch["label"])
+        accu = torchmetrics.functional.accuracy(y_hat, batch["label"].long())
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_accu", accu, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        """
+        Setup the Adam optimizer. Note, that this function also can return a lr scheduler, which is
+        usually useful for training video models.
+        """
+        return torch.optim.Adam(self.parameters(), lr=1e-5)
+
+
+def train():
+    classification_module = VideoClassificationLightningModule()
+    data_module = THUMOS14DataModule()
+    train, val = data_module.train_dataloader().dataset, data_module.val_dataloader().dataset
+    t = next(val)
+    trainer = pytorch_lightning.Trainer(accelerator="gpu", devices=1, check_val_every_n_epoch=1, max_epochs=1000)
+    trainer.fit(classification_module, data_module.train_dataloader(), data_module.val_dataloader())
+
+
+if __name__ == '__main__':
+    train()
